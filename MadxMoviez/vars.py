a6c8fc87265db54567fc3a4451e9bae3d17b0a82
Tailro <163433007+tailro@users.noreply.github.com>
2024-12-20 19:46:45 +0530

    Update vars.py

--- MadxMoviez/vars.py
+++ MadxMoviez/vars.py
@@ -48,20 +48,11 @@ class Var(object):
     ]
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6762558871").split())
     NO_PORT = bool(getenv("NO_PORT", False))
-    APP_NAME = str(getenv("APP_NAME", "f2l.botsfilesharing"))
+    
+    APP_NAME = str(getenv("APP_NAME", "f2l.botsfilesharing.workers.dev"))
     OWNER_USERNAME = str(getenv("OWNER_USERNAME", "@Heart_thieft"))
     BOT_USERNAME = str(getenv("BOT_USERNAME", "HeartxF2L_Bot"))
-    if "DYNO" in environ:
-        ON_HEROKU = True
-        APP_NAME = str(getenv("APP_NAME", "f2l.botsfilesharing"))
-
-    else:
-        ON_HEROKU = False
-    FQDN = (
-        str(getenv("FQDN", "f2l.botsfilesharing"))
-        if not ON_HEROKU or getenv("FQDN", "")
-        else APP_NAME + ".workers.dev"
-    )
+    
 
     DOMAIN = os.environ.get("DOMAIN", "https://f2l.botsfilesharing.workers.dev/")
 
