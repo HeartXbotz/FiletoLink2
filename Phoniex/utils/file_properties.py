33248d6390200ebc1b8b533f0ee8eda4937a1a6a
Boopathi9795 <boopathim693@gmail.com>
2025-01-14 22:09:03 +0530

    New

--- Phoniex/utils/file_properties.py
+++ Phoniex/utils/file_properties.py
@@ -3,7 +3,7 @@ from typing import Any, Optional
 from pyrogram.types import Message
 from pyrogram.file_id import FileId
 from pyrogram.raw.types.messages import Messages
-from MadxMoviez.server.exceptions import FIleNotFound
+from Phoniex.server.exceptions import FIleNotFound
 
 
 async def parse_file_id(message: "Message") -> Optional[FileId]:
