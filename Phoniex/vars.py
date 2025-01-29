71e24c007ecadb52facac58fc882a6f1eb8ebbf7
Jeeva8874 <110565010+Jeeva8874@users.noreply.github.com>
2025-01-30 00:13:15 +0530

    Update vars.py

--- Phoniex/vars.py
+++ Phoniex/vars.py
@@ -49,7 +49,7 @@ class Var(object):
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6762558871 1397269319").split())
     NO_PORT = bool(getenv("NO_PORT", False))
     
-    APP_NAME = str(getenv("APP_NAME", "phoneix.koyeb.app"))
+    APP_NAME = str(getenv("APP_NAME", "f2l.botsfilesharing.workers.dev"))
     OWNER_USERNAME = str(getenv("OWNER_USERNAME", "@Heart_thieft"))
     BOT_USERNAME = str(getenv("BOT_USERNAME", "HeartxF2L_Bot"))
     
